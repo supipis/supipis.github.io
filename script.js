48d0be788881f336add74fe8bfb07f7077e7ade2
Supipi Silva <nirmanialgama@gmail.com>
2020-11-10 23:56:00 +0100

    basic html and css works

---
+++ script.js
@@ -0,0 +1,36 @@
+fetch("header.html").then(res => {
+  return res.text();
+}).then(headerHtml => {
+  const header = document.getElementById('header-container');
+  header.innerHTML = headerHtml;
+})
+
+function clearInput(){
+  document.getElementById("contactme-form").reset();
+}
+
+
+
+setTimeout(function start (){
+  
+    $('.bar').each(function(i){  
+      var $bar = $(this);
+      $(this).append('<span class="count"></span>')
+      setTimeout(function(){
+        $bar.css('width', $bar.attr('data-percent'));      
+      }, i*100);
+    });
+   
+  $('.count').each(function () {
+      $(this).prop('Counter',0).animate({
+          Counter: $(this).parent('.bar').attr('data-percent')
+      }, {
+          duration: 2000,
+          easing: 'swing',
+          step: function (now) {
+              $(this).text(Math.ceil(now) +'%');
+          }
+      });
+  });
+   
+  }, 500)
